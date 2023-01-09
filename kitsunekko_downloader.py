b261cc7dd2973f65b3c4e0193fd01f32f59e1028
sqrtNOT <77981959+sqrtNOT@users.noreply.github.com>
2023-01-09 07:55:20 +0000

    black

--- kitsunekko_downloader.py
+++ kitsunekko_downloader.py
@@ -6,50 +6,52 @@ from bs4 import BeautifulSoup
 import shutil
 
 # create a directory to store the files
-if not os.path.exists('kitsunekko'):
-	os.mkdir('kitsunekko')
-os.chdir('kitsunekko')
+if not os.path.exists("kitsunekko"):
+    os.mkdir("kitsunekko")
+os.chdir("kitsunekko")
 
 # seed the parser with a url
 # accepts any of the dirlist.php urls
-urls_todo = ['https://kitsunekko.net/dirlist.php?dir=subtitles%2Fjapanese%2F']
+urls_todo = ["https://kitsunekko.net/dirlist.php?dir=subtitles%2Fjapanese%2F"]
 download_links = []
 
 # scrape urls recursively
 while urls_todo:
-	print("rate limiting for 5 seconds")
-	time.sleep(5)
-	url = urls_todo.pop()
-	print(f"processing url: {url}")
-	ret = requests.get(url)
-	soup = BeautifulSoup(ret.text, 'html.parser')
-	# use the fact that every page on kitsunekko presents files in the same table format
-	for tag in soup.body.table.find_all("tr"):
-		url = (tag.contents[0].contents[0]['href']).lstrip('/')
-		url = 'https://www.kitsunekko.net/'+url
-		if 'dirlist' in url:
-			urls_todo.append(url)
-		else:
-			download_links.append(url)
+    print("rate limiting for 5 seconds")
+    time.sleep(5)
+    url = urls_todo.pop()
+    print(f"processing url: {url}")
+    ret = requests.get(url)
+    soup = BeautifulSoup(ret.text, "html.parser")
+    # use the fact that every page on kitsunekko presents files in the same table format
+    for tag in soup.body.table.find_all("tr"):
+        url = (tag.contents[0].contents[0]["href"]).lstrip("/")
+        url = "https://www.kitsunekko.net/" + url
+        if "dirlist" in url:
+            urls_todo.append(url)
+        else:
+            download_links.append(url)
 
 # prepare the directory structure to preserve the kitsunekko file hierarchy then download
 for link in download_links:
-	full_path = link.split('/')[5:]
+    full_path = link.split("/")[5:]
 
-	# skip previously downloaded files
-	if os.path.exists('/'.join(full_path)):
-		continue
+    # skip previously downloaded files
+    if os.path.exists("/".join(full_path)):
+        continue
 
-	# create directory structure
-	for index in range(1, len(full_path)):
-		partialpath = ('/'.join(full_path[0:index]))
-		if not os.path.exists(partialpath):
-			os.mkdir(partialpath)
+    # create directory structure
+    for index in range(1, len(full_path)):
+        partialpath = "/".join(full_path[0:index])
+        if not os.path.exists(partialpath):
+            os.mkdir(partialpath)
 
-	# stream the download directly to a file
-	with open('/'.join(full_path), 'wb') as f:
-		print("rate limiting for 5 seconds")
-		time.sleep(5) # sleep after rate limit message so stopping early can be done cleanly
-		print(f"Downloading new file: {'/'.join(full_path)}")
-		with requests.get(link, stream=True) as r:
-			shutil.copyfileobj(r.raw, f)
+    # stream the download directly to a file
+    with open("/".join(full_path), "wb") as f:
+        print("rate limiting for 5 seconds")
+        time.sleep(
+            5
+        )  # sleep after rate limit message so stopping early can be done cleanly
+        print(f"Downloading new file: {'/'.join(full_path)}")
+        with requests.get(link, stream=True) as r:
+            shutil.copyfileobj(r.raw, f)
